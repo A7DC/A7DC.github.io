8b1d4fcc9d6a9692b86cbf88a9f4690e774c69c9
Daniel Caine <Daniel@Daniels-MacBook-Pro.local>
2015-10-04 22:55:10 +1300

    added slick slider

---
+++ javascript/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	// $('.work-holder').slick({
+	//   infinite: false,
+	//   prevArrow: true,
+	//   nextArrow: true,
+	//   slidesToShow: 3,
+	//   slidesToScroll: 3
+	// });
+	$('.slider').slick();
+});
+
