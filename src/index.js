ba84ac9d1804656fc031cf5a83088683a107a40d
Daniel Caine <audaci7y@gmail.com>
2018-05-17 15:51:27 +1000

    switching between routes

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import {Navigation} from './components/Navigation';
+import {Home} from './components/Home';
+import {About} from './components/About';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path="/" component={Navigation} />
+      <Navigation />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
     </div>
   </Router>,
   document.getElementById('root')
