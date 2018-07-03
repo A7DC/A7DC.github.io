16b0369b7e17d9a75c873c2d129e15e5e51416c2
Daniel Caine <audaci7y@gmail.com>
2018-07-04 11:31:32 +1200

    opacity

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -17,17 +17,9 @@ const fast = { ...config.stiff, restSpeedThreshold: 1, restDisplacementThreshold
 // Creates a spring with predefined animation slots
 const Sidebar = Keyframes.Spring({
   // Slots can take arrays/chains,
-  peek: [
-    { delay: 500, from: { x: -100 }, to: { x: 0 }, config: fast },
-    { delay: 800, to: { x: -100 }, config: config.slow }
-  ],
-  // single items,
-  open: { to: { x: 0 }, config: config.default },
-  // or async functions with side-effects
-  close: async call => {
-    await delay(400)
-    await call({ to: { x: -100 }, config: config.gentle })
-  }
+  start: [
+    { delay: 500, from: { x: 0 }, to: { x: 1 }, config: config.slow },
+  ]
 })
 
 
@@ -62,16 +54,31 @@ const Sidebar = Keyframes.Spring({
 class Home extends Component {
   state = { open: undefined }
   render() {
-    const state = this.state.open === undefined ? 'peek' : this.state.open ? 'open' : 'close'
+    const state = this.state.open === undefined ? 'start' : this.state.open ? 'open' : 'close'
+    const { style } = this.props;
     return (
-      <Sidebar native state={state}>
-        {({ x }) => (
-          <animated.div className="sidebar" style={{ transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }}>
-            <h1 className={classes.h1}>Daniel Caine is a full-stack designer from England</h1>
-            <p className={classes.subtitle}>Currently: Bordellio. Previously: SilverStripe, Levo and Reapit. <br /> Send me an <a href="#" className="underline color-inherit">email</a>, check my <a href="#" className="underline color-inherit">LinkedIn</a> profile, or download my <a href="#" className="underline color-inherit">CV</a>. <br /> Below is a selection of my work.</p>
-          </animated.div>
-        )}
-      </Sidebar>
+    <div 
+      className="mainRoute" 
+    style={
+      { ...style, 
+        backgroundColor: '#FFF87A',
+        color: '#2b2b2b'
+        }
+      }>
+      <ContentContainer>
+        <div className="mw8 pl6 pt6 mt6">
+          <Sidebar native state={state}>
+            {({ x }) => (
+              <animated.div className="sidebar" style={{ opacity: x.interpolate(x => x) }}>
+                <h1 className={classes.h1}>Daniel Caine is a full-stack designer from England</h1>
+                <p className={classes.subtitle}>Currently: Bordellio. Previously: SilverStripe, Levo and Reapit. <br /> Send me an <a href="#" className="underline color-inherit">email</a>, check my <a href="#" className="underline color-inherit">LinkedIn</a> profile, or download my <a href="#" className="underline color-inherit">CV</a>. <br /> Below is a selection of my work.</p>
+              </animated.div>
+            )}
+          </Sidebar>
+          <ArrowDown />
+        </div>
+      </ContentContainer>
+    </div>
     )
   }
 }
