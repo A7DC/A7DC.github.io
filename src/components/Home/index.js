8ef698804225a8f0b27479ffe5e2eb2d497e7898
Daniel Caine <audaci7y@gmail.com>
2018-07-04 13:21:42 +1200

    tidy up

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,66 +15,12 @@ const classes = {
 const fast = { ...config.stiff, restSpeedThreshold: 10, restDisplacementThreshold: 0.01 }
 
 // Creates a spring with predefined animation slots
-const Sidebar = Keyframes.Spring({
+const Content = Keyframes.Spring({
   // Slots can take arrays/chains,
   start: [
-    { delay: 400, from: { x: 0 }, to: { x: 1 }, config: config.slow },
+    { delay: 400, from: { x: 0 }, to: { x: 1 }, config: fast },
   ]
 })
-
-
-
-// export const Home = style => (
-//   <div 
-//     className="mainRoute" 
-//   style={
-//     { ...style, 
-//       backgroundColor: '#FFF87A',
-//       color: '#2b2b2b'
-//       }
-//     }>
-//     <ContentContainer>
-//       <div className="mw8 pl6 pt6 mt6">
-//         <Sidebar native state={state}>
-//           {({ x }) => (
-//             <animated.div className="sidebar" style={{ transform: x.interpolate(x => `translate3d(${x}%,0,0)`) }}>
-//               <h1 className={classes.h1}>Daniel Caine is a full-stack designer from England</h1>
-//               <p className={classes.subtitle}>Currently: Bordellio. Previously: SilverStripe, Levo and Reapit. <br /> Send me an <a href="#" className="underline color-inherit">email</a>, check my <a href="#" className="underline color-inherit">LinkedIn</a> profile, or download my <a href="#" className="underline color-inherit">CV</a>. <br /> Below is a selection of my work.</p>
-//             </animated.div>
-//           )}
-//         </Sidebar>
-//         <ArrowDown />
-//       </div>
-//     </ContentContainer>
-//   </div>
-// )
-
-
-// export class Home extends Component {
-//   render() {
-//     const { style } = this.props;
-//     return (
-//       <div
-//         className="mainRoute"
-//         style={
-//           {
-//             ...style,
-//             backgroundColor: '#FFF87A',
-//             color: '#2b2b2b'
-//           }
-//         }>
-//         <ContentContainer>
-//           <div className="mw8 pl6 pt6 mt6">
-//             <h1 className={classes.h1}>Daniel Caine is a full-stack designer from England</h1>
-//             <p className={classes.subtitle}>Currently: Bordellio. Previously: SilverStripe, Levo and Reapit. <br /> Send me an <a href="#" className="underline color-inherit">email</a>, check my <a href="#" className="underline color-inherit">LinkedIn</a> profile, or download my <a href="#" className="underline color-inherit">CV</a>. <br /> Below is a selection of my work.</p>
-//             <ArrowDown />
-//           </div>
-//         </ContentContainer>
-//       </div>
-//     )
-//   }
-// }
-
 
 class Home extends Component {
   state = { open: undefined }
@@ -92,14 +38,14 @@ class Home extends Component {
       }>
       <ContentContainer>
         <div className="mw8 pl6 pt6 mt6">
-          <Sidebar native state={state}>
+            <Content native state={state}>
             {({ x }) => (
               <animated.div className="sidebar" style={{ opacity: x.interpolate(x => x) }}>
-                <h1 className={classes.h1}>Daniel Caine is a full-stack designer from England</h1>
+                <h1 className={classes.h1}>Daniel Caine is a full-stack <br /> designer from England</h1>
                 <p className={classes.subtitle}>Currently: Bordellio. Previously: SilverStripe, Levo and Reapit. <br /> Send me an <a href="#" className="underline color-inherit">email</a>, check my <a href="#" className="underline color-inherit">LinkedIn</a> profile, or download my <a href="#" className="underline color-inherit">CV</a>. <br /> Below is a selection of my work.</p>
               </animated.div>
             )}
-          </Sidebar>
+            </Content>
           <ArrowDown />
         </div>
       </ContentContainer>
