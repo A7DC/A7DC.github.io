fbf8f35c3eae5016de425cdfd9a4ee9b77fd80c9
Daniel Caine <audaci7y@gmail.com>
2018-07-28 17:40:45 +1200

    logging window scroll position on homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,22 @@ import Hero from '../Hero'
 import Work from '../Work'
 
 class Home extends Component {
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+
+  handleScroll(e) {
+    const windowScrollPosition = window.scrollY
+    console.log('windowScrollPosition', windowScrollPosition)
+  }
+
+
   render() {
     return (
       <Fragment>
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 let Background =  'images/bordellio-work-bg.png'
 
 const classes = {
-  container: 'relative',
+  container: 'relative white vh-100',
   images: 'absolute left-0 right-0 bottom-0 top-0 z-0',
   h6: 'f1-ns f6 ma0 pb3',
   subtitle: 'f4-ns f4 lh-copy ma0 mb4-ns mb6',
+  content: 'mw8 ph3 pl6-ns pr0-ns absolute bottom-2'
 }
 
 const Work = () => (
@@ -15,7 +16,7 @@ const Work = () => (
       backgroundImage: `url(${Background})`
     }}
     >
-    <div className="z-max">
+    <div className={classes.content}>
       <h6 className={classes.h6}>Bordellio</h6>
       <p className={classes.subtitle}>A digitial transformation of the sex industry</p>
     </div>
