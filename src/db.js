7d395ccc312f339494a63e7011fa3e87562bc9a4
Daniel Caine <audaci7y@gmail.com>
2018-08-08 18:56:59 +1200

    spelling correction

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const data = {
   workHero: { title: 'A digitial transformation of the sex industry', subtitle: 'Bordellio', bg: 'images/bordellio-work-bg.png' },
   work: [
-    { title: 'Ui Work', subtitle: 'Dribbble', bg: 'images/dribbble-bg.png' },
+    { title: 'UI Work', subtitle: 'Dribbble', bg: 'images/dribbble-bg.png' },
     { title: 'Medium.com', subtitle: 'Writing', bg: 'images/medium-bg.png' }
   ],
 }
