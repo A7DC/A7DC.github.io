3e6ded09e7caafe0673c18b45b32ec274a9af0b6
Daniel Caine <audaci7y@gmail.com>
2018-08-22 13:44:49 +1200

    Loading text animating in

--- src/components/LoadingAnimation/index.js
+++ src/components/LoadingAnimation/index.js
@@ -1,37 +1,36 @@
 import React, { Component } from 'react'
-import { Trail, animated } from 'react-spring'
-import { heading, stucture } from '../../tachyons-classes'
+import { Trail, animated, config } from 'react-spring'
+import { heading } from '../../tachyons-classes'
 
 const classes = {
   container: 'w-100 vh-100 flex justify-center items-center bg-white content-box'
 }
 
 class LoadingAnimation extends Component {
-  state = { toggle: true, items: ['item1', 'item2', 'item3', 'item4', 'item5'] }
-
-  toggle = () => this.setState(state => ({ toggle: !state.toggle }))
+  state = { 
+    toggle: true, 
+    items: ['Designer,', 'programmer,', 'founder.']
+  }
 
   render() {
     const { toggle, items } = this.state
     return (
       <div className={classes.container}>
-        <h2 className={heading.title}>Designer. Developer.</h2>
-
         <Trail
+          config={config.gentle} 
           native
-          from={{ opacity: 0, x: -100 }}
-          to={{ opacity: toggle ? 1 : 0.25, x: toggle ? 0 : 100 }}
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: toggle ? 1 : 0, y: toggle ? 0 : 40 }}
           keys={items}>
-          {items.map(item => ({ x, opacity }) => (
+          {items.map((_, i) => ({ y, opacity }) => (
             <animated.div
               className="box"
-              onClick={this.toggle}
               style={{
                 opacity,
-                transform: x.interpolate(x => `translate3d(${x}%,0,0)`)
+                transform: y.interpolate(y => `translate3d(0,${y}%,0)`)
               }}
             >
-              {"example"}
+              <h2 className={`${heading.t2} fw6 pr2`}>{items[i]}</h2>
             </animated.div>
           ))}
         </Trail>
