87d1ef8a16d8b9fe1e15d3c8a9acefc43f18a35a
Daniel Caine <audaci7y@gmail.com>
2018-08-23 10:23:10 +1200

    styled corretly

--- src/components/LoadingAnimation/index.js
+++ src/components/LoadingAnimation/index.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import { Keyframes, animated, config } from 'react-spring'
+import { heading } from '../../tachyons-classes';
 
 const classes = {
   container: 'w-100 vh-100 flex justify-center items-center bg-white content-box'
 }
 
 const Content = Keyframes.Trail({
-  start: [{ delay: 600, from: { x: -100, opacity: 0 }, to: { x: 0, opacity: 1 } }, { to: { x: -100, opacity: 0 } }]
+  start: [{ delay: 600, from: { y: 100, opacity: 0 }, to: { y: 0, opacity: 1 } }, { to: { y: 100, opacity: 0 } }]
 })
 
 class LoadingAnimation extends Component {
   state = { 
     start: undefined,
-    items: ['Designer,', 'programmer,', 'founder.']
+    items: ['Designer, ', 'programmer, ', 'founder.']
   }
 
   render() {
@@ -20,14 +21,14 @@ class LoadingAnimation extends Component {
     const state = this.state.start === undefined ? 'start' : null
     return (
         <div className={classes.container}>
-          <Content native keys={items.map((_, i) => i)} config={{ tension: 200, friction: 20 }} state={state}>
-            {items.map((item, i) => ({ x, ...props }) => (
+        <Content native keys={items.map((_, i) => i)} state={state} config={config.gentle}>
+            {items.map((item) => ({ y, ...props }) => (
               <animated.div
                 style={{
-                  transform: x.interpolate(x => `translate3d(${x}%,0,0)`),
+                transform: y.interpolate(y => `translate3d(0,${y}%,0)`),
                   ...props
                 }}>
-                {item}
+              <h2 className={`${heading.t2} fw6 pr2`}>{item}</h2>
               </animated.div>
             ))}
           </Content>
