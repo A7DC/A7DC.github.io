23aa86957b58955df66bf5ed67f3ac1946f43ab7
Daniel Caine <audaci7y@gmail.com>
2018-08-28 11:57:04 +1200

    began case studies page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 // components
 import Home from '../Home';
-import { About } from '../About';
+import Bordellio from '../Bordellio';
 import LoadingAnimation from '../LoadingAnimation';
 
-// styles
-import './styles.css'
-
-export const App = ({location}) => (
+export const App = () => (
     <Router>
       <Route
         render={({ location}) => (
@@ -18,12 +15,11 @@ export const App = ({location}) => (
               exact 
               path="/" 
               />
-            {/* <Header /> */}
           <div>
             <Switch location={location}>
-              <Route exact path='/' render={props => <Home />} />
-              <Route exact path='/loading' render={props => <LoadingAnimation />} />
-              <Route exact path="/about" render={props => About()} />
+              <Route exact path='/' render={() => <Home />} />
+              <Route exact path='/bordellio' render={() => <Bordellio />} />
+              <Route exact path='/loading' render={() => <LoadingAnimation />} />
               <Route render={() => <div>Not Found</div>} />
             </Switch>
             </div>
