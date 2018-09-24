366cd70cd28bfbf2a13a65c4e86f8ddedf31c44a
Daniel Caine <audaci7y@gmail.com>
2018-09-24 15:36:14 +1200

    skills is now a functional component

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,47 +1,37 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { heading } from '../../tachyons-classes'
 
 import data from '../../db'
 import AnimationContainer from '../AnimationContainer';
 import ContentContainer from '../ContentContainer';
 
-class Skills extends Component {
-
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <ContentContainer>
-        <section ref={r => this.container = r} className="relative white pv6">
-          <div className='ph2 ph5-ns w-100 h-100 mw9 mla mra relative'>
-            <div className="flex flex-row-ns flex-column items-baseline">
-              <div className="w-100 w-40-ns fl mb0-ns mb3">
-                <h3 className={heading.t2}>Skills</h3>
-              </div>
-              <div className="w-100 w-60-ns fl">
-                {data.skills.map((skill, i) => {
-                  return (
-                    <div key={i} className="w-100 flex flex-row items-baseline pb5 pb0-lc">
-                      <span className='w-20'>
-                        <h4 className={heading.subtitle}>{skill.title}</h4>
-                      </span>
-                      <span className='w-80'>
-                        <p className={heading.p}>{skill.content}</p>
-                      </span>
-                    </div>
-                  )
-                })}
-              </div>
-            </div>
+const Skills = () => (
+  <ContentContainer>
+    <section ref={r => this.container = r} className="relative white pv6">
+      <div className='ph2 ph5-ns w-100 h-100 mw9 mla mra relative'>
+        <div className="flex flex-row-ns flex-column items-baseline">
+          <div className="w-100 w-40-ns fl mb0-ns mb3">
+            <h3 className={heading.t2}>Skills</h3>
           </div>
-        </section>
-      </ContentContainer>
-    )
-  }
-
-}
+          <div className="w-100 w-60-ns fl">
+            {data.skills.map((skill, i) => {
+              return (
+                <div key={i} className="w-100 flex flex-row items-baseline pb5 pb0-lc">
+                  <span className='w-20'>
+                    <h4 className={heading.subtitle}>{skill.title}</h4>
+                  </span>
+                  <span className='w-80'>
+                    <p className={heading.p}>{skill.content}</p>
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
+  </ContentContainer>
+)
 
 const SkillsAnimated = AnimationContainer(Skills)
 
