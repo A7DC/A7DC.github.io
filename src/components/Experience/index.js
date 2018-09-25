5091885de8ca07598980e10a38c8dc6f6887762e
Daniel Caine <audaci7y@gmail.com>
2018-09-25 13:44:38 +1200

    experience styling for mobile and ipad

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -5,19 +5,23 @@ import AnimationContainer from '../AnimationContainer'
 
 import data from '../../db'
 
+const classes = {
+  col: 'w-100 w-50-l fl'
+}
+
 const Experience = () => (
   <section className='ph2 ph5-ns w-100 h-100 mw9 mla mra relative'>
     <ContentContainer>
       <div className="flex flex-row pv6">
         <div className={structure.containerInner}>
-          <div className="w-50 fl">
-            <h3 className={typography.t2}>Experience</h3>
+          <div className={classes.col}>
+            <h3 className={`${typography.t2} mb4 mb0-l`}>Experience</h3>
           </div>
-          <div className="w-50 fl">
+          <div className={classes.col}>
             <div className="w-100 pl20pc">
               {data.workExperience.map((work, i) => {
                 return (
-                  <div key={i} className="w-50 fl flex flex-column items-baseline pb5">
+                  <div key={i} className="w-100 w-50-l fl flex flex-column items-baseline pb5">
                     <h6 className={typography.p}><b>{work.employer}</b></h6>
                     <span className={typography.p}>{work.jobTitle}</span>
                     <span className={typography.p}>{work.datesWorked}</span>
