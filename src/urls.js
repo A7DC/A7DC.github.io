3acab431e1e87dd697213ccd54b81adabe7651ce
Daniel Caine <audaci7y@gmail.com>
2018-10-12 09:26:46 +1300

    Updates

--- src/urls.js
+++
@@ -1,9 +0,0 @@
-const urls = {
-  home: '/',
-  bordellio: '/bordellio',
-  linkedin: 'https://www.linkedin.com/in/dancaine/',
-  cv: 'https://docs.google.com/document/d/1AKipWpYUAHtycLxuMur9PsZF7UuxxujUYOFt6JeAeB4/edit?usp=sharing',
-  email: 'mailto: audaci7y@gmail.com'
-}
-
-export default urls
