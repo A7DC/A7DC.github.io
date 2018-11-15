d154900d951fee05247ca010ceb2760d5768f0a1
Daniel Caine <audaci7y@gmail.com>
2018-11-15 15:02:44 +1300

    added google analytics tracking to App

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import { Transition, config } from 'react-spring'
 import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
@@ -14,37 +14,47 @@ import Home from './components/Home'
 import Bordellio from './components/Bordellio'
 import BordellioDev from './components/BordellioDev'
 
-ReactGA.initialize('UA-68646651-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
-const App = () => (
-  <Router>
-    <Route
-      render={({ location, ...rest }) => (
-        <div className="fill sans-serif font-smoothing lh-copy">
-          <Route exact path="/" render={() => <Redirect to="/home" />} />
-          <div className="content">
-            <Transition
-              native
-              config={config.slow}
-              keys={location.pathname.split('/').filter(a => a)[0]}
-              from={{ willChange: 'transform,opacity', 
-                       transform: 'translateY(500px)', opacity: 0 }}
-              enter={{ transform: 'translateY(0px)', opacity: 1 }}
-              leave={{ transform: 'translateX(-500px)', opacity: 0 }}>
-              {style => (
-                <Switch location={location}>
-                  <Route path="/home" render={props => <Home {...props} style={style} />} />
-                  <Route path={urls.bordellioDev} render={props => <BordellioDev {...props} style={style} />} />
-                  <Route path={urls.bordellio} render={props => <Bordellio {...props} style={style} />} />
-                </Switch>
-              )}
-            </Transition>
-          </div>
-        </div>
-      )}
-    />
-  </Router>
-)
+class App extends Component {
+
+  componentDidMount() {
+    ReactGA.initialize('UA-68646651-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Route
+          render={({ location, ...rest }) => (
+            <div className="fill sans-serif font-smoothing lh-copy">
+              <Route exact path="/" render={() => <Redirect to="/home" />} />
+              <div className="content">
+                <Transition
+                  native
+                  config={config.slow}
+                  keys={location.pathname.split('/').filter(a => a)[0]}
+                  from={{
+                    willChange: 'transform,opacity',
+                    transform: 'translateY(500px)', opacity: 0
+                  }}
+                  enter={{ transform: 'translateY(0px)', opacity: 1 }}
+                  leave={{ transform: 'translateX(-500px)', opacity: 0 }}>
+                  {style => (
+                    <Switch location={location}>
+                      <Route path="/home" render={props => <Home {...props} style={style} />} />
+                      <Route path={urls.bordellioDev} render={props => <BordellioDev {...props} style={style} />} />
+                      <Route path={urls.bordellio} render={props => <Bordellio {...props} style={style} />} />
+                    </Switch>
+                  )}
+                </Transition>
+              </div>
+            </div>
+          )}
+        />
+      </Router>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
